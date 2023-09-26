5f5cbf25c4a3922da7fe396f996669861f8a8075
O-Haruki <134474921+O-Haruki@users.noreply.github.com>
2023-09-26 17:44:08 +0900

    [Update] Enemy_script Z軸の回転の追加

--- Assets/Okamura/Enemy.cs
+++ Assets/Okamura/Enemy.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.ComponentModel;
 using UnityEngine;
+using DG.Tweening;
+
 
 [RequireComponent(typeof(Animator))]
 public class Enemy : MonoBehaviour
@@ -15,17 +17,21 @@ public class Enemy : MonoBehaviour
     [SerializeField] float _impactedSpeed = 3;
     [Tooltip("エネミーの落下速度")]
     [SerializeField] float _gravity = 0.2f;
+    [Tooltip("エネミーの1回転にかかる時間")]
+    [SerializeField] float _rotateTime = 0.2f;
     [Tooltip("エネミーの吹っ飛び方")]
     [SerializeField] Vector2[] _vec2 = new[] { new Vector2(1, 0), new Vector2(1, 1), new Vector2(1, -1)};
     /// <summary>
     /// エネミーにつけるAnimatorのパラメーターにはbool値"Defeat"をfalseで追加してください
     /// </summary>
+    Transform _tra;
     Rigidbody2D _rb;
     Animator _anim;
     private void Start()
     {
         _anim = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
+        _tra = GetComponent<Transform>();
     }
 
     private int GetScore()
@@ -68,6 +74,7 @@ public class Enemy : MonoBehaviour
     {
         int score = GetScore();
         _anim.SetBool("Defeat", true);
+        _tra.DORotate(new Vector3(0, 0, 360), _rotateTime, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1);
         _rb.velocity = (_vec2[GetState()].normalized * _impactedSpeed);
         //_rb.AddForce(_vec2[GetState()].normalized * _impactedSpeed,ForceMode2D.Impulse);
         _rb.gravityScale = _gravity;
