1e4ade11f103de791713af54291feb9ce37a56ce
O-Haruki <134474921+O-Haruki@users.noreply.github.com>
2023-09-26 15:47:41 +0900

    [Add] Enemy_script 敵を吹っ飛ばす処理の追加、scene テスト用のobjectを追加

--- Assets/Okamura/Enemy.cs
+++ Assets/Okamura/Enemy.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.ComponentModel;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
@@ -10,13 +11,19 @@ public class Enemy : MonoBehaviour
     [SerializeField] int _enemyScoreC = 1000;
     [Tooltip("EnemyのDestroyされるまでの時間")]
     [SerializeField] float _waitDestroy = 1;
+    [Tooltip("エネミーの吹っ飛ぶ速度")]
+    [SerializeField] float _impactedSpeed = 3;
+    [Tooltip("エネミーの吹っ飛び方")]
+    [SerializeField] Vector2[] _vec2 = new[] { new Vector2(1, 0), new Vector2(1, 1), new Vector2(1, -1)};
     /// <summary>
     /// エネミーにつけるAnimatorのパラメーターにはbool値"Defeat"をfalseで追加してください
     /// </summary>
+    Rigidbody2D _rb;
     Animator _anim;
     private void Start()
     {
         _anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
     }
     private int GetScore()
     {
@@ -33,6 +40,21 @@ public class Enemy : MonoBehaviour
             return _enemyScoreC;
         }
     }
+    private int GetState()
+    {
+        if (_state == EnemyState.EnemyA)
+        {
+            return 0;
+        }
+        else if (_state == EnemyState.EnemyB)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
 
     [Tooltip("エネミーを倒す時に呼ぶ")]
     public void Defeated()
@@ -42,10 +64,11 @@ public class Enemy : MonoBehaviour
     private IEnumerator IDefeated()
     {
         int score = GetScore();
-        GameManager.Instance.ScoreValue(score);
-        yield return new WaitForSeconds(_waitDestroy);
-        //ここからanimatorの処理を描く
         _anim.SetBool("Defeat", true);
+        _rb.AddForce(_vec2[GetState()].normalized * _impactedSpeed,ForceMode2D.Impulse);
+        GameManager.Instance.ScoreValue(score);
+        //ここからanimatorの処理を描く
+        yield return new WaitForSeconds(_waitDestroy);
         Destroy(this);
     }
 }
