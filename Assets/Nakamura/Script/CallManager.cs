b6dda885f93d0b38aa612ae7c02ae68365065978
NakamuraTakuto <rulitaku@icloud.com>
2023-09-27 10:53:23 +0900

    [Update] GameManager (ループ時のエラー解決)

--- Assets/Nakamura/Script/CallManager.cs
+++ Assets/Nakamura/Script/CallManager.cs
@@ -7,7 +7,6 @@ public class CallManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Call");
         GameManager.Instance.LoadProssesing();
     }
 }
