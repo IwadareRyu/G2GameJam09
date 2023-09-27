6b461af6497ca1ee5089ccc2b605f3c5ecc715ac
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-09-27 14:46:05 +0900

    [add] SystemUICall

---
+++ Assets/Nakamura/Script/SystemUICall.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemUI : MonoBehaviour
+{
+    [Tooltip("Score、Timeを表示するテキストの背景UI")]
+    [SerializeField] private GameObject _systemUI = null;
+    private bool is_UI = false;
+    private void Start()
+    {
+        if (_systemUI != null) {_systemUI.SetActive(false);}
+        else {Debug.Log("シリアライズしてください");}
+    }
+
+    void Update()
+    {
+        if (!is_UI && GameManager.Instance.is_Game && _systemUI != null)
+        {
+            is_UI = true;
+            _systemUI.SetActive((true));
+        }
+    }
+}
